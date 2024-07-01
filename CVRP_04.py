6ad4f127b2c4f00b32db84c295772615c2c0bcaf
Pablo Carmona <pablocarben@outlook.com>
2024-07-01 17:11:06 +0200

    Backup july 1st 2024

--- CVRP_04.py
+++ CVRP_04.py
@@ -1,5 +1,10 @@
 # Building CVRP solver using metaheuristics from scratch.
 
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
 def read_txt_file(filename):
     """
     Read coordinates and demand values from a .txt file.
@@ -64,11 +69,60 @@ def total_distance(route, diction):
         total += distance(diction[route[i]], diction[route[i+1]])
     return total
 
+# We are meant to change this objective function according to CVRP optimization objectives
 def objective_function(solution, diction):
     return total_distance(solution, diction)
-    
 
+# Distance matrix makes it computationally faster to calculate the distance between two points.
+def calculate_distance_matrix(coordinates):
+    """
+    Calculate the distance matrix between coordinates.
+    """
+    num_points = len(coordinates)
+    dist_matrix = np.zeros((num_points, num_points))
 
+    for i in range(num_points):
+        for j in range(num_points):
+            dist_matrix[i, j] = calculate_distance(coordinates, i, j)
+
+    return dist_matrix
+
+# Review if distance is correctly calculated.
+# This function works pretty much the same as distance_euclidean_AB
+def calculate_distance(coordinates, i, j):
+    """
+    Calculate the Euclidean distance between two points.
+    """
+    x1, y1 = coordinates[i]
+    x2, y2 = coordinates[j]
+    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+# Review if total distance is correctly calculated
+def calculate_total_distance(route, dist_matrix):
+    """
+    Calculate the total distance of a given route using the distance matrix.
+    """
+    total_distance = 0
+    num_points = len(route)
+
+    for i in range(num_points - 1):
+        current_node = route[i]
+        next_node = route[i + 1]
+        total_distance += dist_matrix[current_node, next_node]
+
+    return total_distance
+
+def plot_problem(dic_customers: dict):
+    customers = list(dic_customers.keys()) # List of customers to visit
+    X = [dic_customers[customer][0] for customer in customers] # List of X coordenates
+    Y = [dic_customers[customer][1] for customer in customers] # List of Y coordenates
+    X.append(X[0]) # Initial position X 
+    Y.append(Y[0]) # Initial position Y
+    plt.plot(X,Y,'o')
+    for customer in customers:  # Show customer number on each customer
+        plt.annotate(customer, (dic_customers[customer][0]+0.01, dic_customers[customer][1]))
+
+# Have in mind: auxiliary functions shall be put in a separate file for cleanness.
 
 # Main function
 def main():
@@ -76,6 +130,10 @@ def main():
     veh_num, capacity, custnum, coordx, coordy, demand, readytime, duedate, servicetime = read_txt_file("C:\\Users\\Pablo\\OneDrive - UNIVERSIDAD DE SEVILLA\\MOIGE\\CUATRI2\\MOPG\\Python\\07 CVRP\\CVRP\\Datos.txt")
 
     dict_xy = {custnum[i]: (coordx[i], coordy[i]) for i in range(len(custnum))}
+
+    # Plotting the problem
+    plot_problem(dict_xy)
+    plt.show()
     
 
 # Run the main function
